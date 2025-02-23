8bc9a907884b5864bd5f8639044988e241fbc5b1
sunshine-walker-93 <93maoshui.li@gmail.com>
2025-02-23 16:08:24 +0800

    更新protoc-gen-mirco的版本

--- protobuf3.pb/user_info_manage/user_info.pb.micro.go
+++ protobuf3.pb/user_info_manage/user_info.pb.micro.go
@@ -5,15 +5,14 @@ package user_info_manage
 
 import (
 	fmt "fmt"
-	proto "github.com/golang/protobuf/proto"
+	proto "google.golang.org/protobuf/proto"
 	math "math"
 )
 
 import (
 	context "context"
-	api "github.com/micro/micro/v3/service/api"
-	client "github.com/micro/micro/v3/service/client"
-	server "github.com/micro/micro/v3/service/server"
+	client "go-micro.dev/v5/client"
+	server "go-micro.dev/v5/server"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -21,23 +20,10 @@ var _ = proto.Marshal
 var _ = fmt.Errorf
 var _ = math.Inf
 
-// This is a compile-time assertion to ensure that this generated file
-// is compatible with the proto package it is being compiled against.
-// A compilation error at this line likely means your copy of the
-// proto package needs to be updated.
-const _ = proto.ProtoPackageIsVersion3 // please upgrade the proto package
-
 // Reference imports to suppress errors if they are not otherwise used.
-var _ api.Endpoint
 var _ context.Context
 var _ client.Option
 var _ server.Option
-
-// Api Endpoints for UserService service
-
-func NewUserServiceEndpoints() []*api.Endpoint {
-	return []*api.Endpoint{}
-}
 
 // Client API for UserService service
 
